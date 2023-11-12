f843890df5c62f37570a1f3e158b63909438ebe7
Anatoliy Ch <50632903+ch3pasov@users.noreply.github.com>
2023-11-12 17:08:07 +0100

    Теперь радио вечно стримит вечный стрим

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pyrogram
 from pytgcalls import PyTgCalls
-from pytgcalls.mtproto.pyrogram_client import PyrogramClient
 from pytgcalls import idle
 from pytgcalls.types import (
     AudioPiped,
@@ -9,7 +8,10 @@ from pytgcalls.types import (
     AudioQuality,
     # VideoParameters,
     # VideoQuality,
+    # Update,
 )
+import asyncio
+# from random import randint
 import secret
 
 api_id = secret.api_id
@@ -21,39 +23,67 @@ join_channel_id = secret.join_channel_id
 join_access_hash = secret.join_access_hash
 
 app = pyrogram.Client("my_account", api_id, api_hash)
-calls_app_simple = PyTgCalls(app)
-calls_app_difficult = PyrogramClient(1, app)
-calls_app_simple.start()
 
-# # 1291274978
-# # {
-# #     "_": "types.InputPeerChannel",
-# #     "channel_id": 1291274978,
-# #     "access_hash": 1446172399040448336
-# # }
-# print(app.resolve_peer(1291274978))
-
-calls_app_simple.join_group_call(
+app_calls = PyTgCalls(app)
+app_calls.start()
+app_calls.join_group_call(
     channel_id,
     join_as=pyrogram.raw.types.InputPeerChannel(channel_id=join_channel_id, access_hash=join_access_hash)
-)
-
-calls_app_simple.change_stream(
-    channel_id,
-    AudioPiped(
-        'audio.mp3',
-        audio_parameters=AudioParameters.from_quality(AudioQuality.HIGH),
-    )
 )
 
 print(1)
 
 
+# USE THIS IF YOU WANT SYNC WAY
+def get_youtube_stream():
+    # USE THIS IF YOU WANT ASYNC WAY
+    async def run_async():
+        proc = await asyncio.create_subprocess_exec(
+            'yt-dlp',
+            '-g',
+            '-f',
+            # 'best[height<=?720][width<=?1280]',
+            'ba',  # best audio
+            'https://www.youtube.com/live/jfKfPfyJRdk?si=gSkWIK09MCq5WuT1',
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await proc.communicate()
+        return stdout.decode().split('\n')[0]
+    return asyncio.get_event_loop().run_until_complete(run_async())
+
+
+remote = get_youtube_stream()
+
+
+app_calls.change_stream(
+    channel_id,
+    AudioPiped(
+        remote,
+        audio_parameters=AudioParameters.from_quality(AudioQuality.HIGH),
+    )
+)
+
+
+# @app_calls.on_stream_end()
+# async def handler(client: PyTgCalls, update: Update):
+#     print(update)
+#     await app_calls.change_stream(
+#         channel_id,
+#         AudioPiped(
+#             'audio_02.mp3',
+#             audio_parameters=AudioParameters.from_quality(AudioQuality.HIGH),
+#         )
+#     )
+
+
+# главный обработчик событий в войсчате
 @app.on_raw_update()
 async def raw(client, update, users, chats):
     # print(type(update))
     # print(dir(update))
     if type(update) is pyrogram.raw.types.update_group_call_participants.UpdateGroupCallParticipants:
+        call = update.call
         for participant in update.participants:
             # print(participant)
             match type(participant.peer):
@@ -61,27 +91,47 @@ async def raw(client, update, users, chats):
                     participant_type = 'user'
                     participant_id = participant.peer.user_id
                 case pyrogram.raw.types.PeerChat:
+                    return
                     participant_type = 'chat'
                     participant_id = participant.peer.chat_id
                 case pyrogram.raw.types.PeerChannel:
+                    return
                     participant_type = 'channel'
                     participant_id = participant.peer.channel_id
-                    print(update)
+            assert participant_type == 'user'
+
             if participant.left:
                 print(f'{participant_type} {participant_id} left')
             if participant.just_joined:
                 print(f'{participant_type} {participant_id} just joined')
-                if participant_type == 'user':
-                    peer = await app.resolve_peer(participant_id)
-                    await calls_app_difficult.set_call_status(
-                        channel_id,
+                peer = await app.resolve_peer(participant_id)
+                await app.invoke(
+                    pyrogram.raw.functions.phone.EditGroupCallParticipant(
+                        call=call,
                         participant=peer,
-                        muted_status=False,
-                        paused_status=None,
-                        stopped_status=None
+                        muted=False
                     )
+                )
             if participant.raise_hand_rating:
                 print(f'{participant_type} {participant_id} raise hand with rating {participant.raise_hand_rating}')
+                # peer = await app.resolve_peer(participant_id)
+                # await asyncio.sleep(5)
+                # if randint(0, 1):
+                #     await app.invoke(
+                #         pyrogram.raw.functions.phone.EditGroupCallParticipant(
+                #             call=call,
+                #             participant=peer,
+                #             raise_hand=False
+                #         )
+                #     )
+                # else:
+                #     await app.invoke(
+                #         pyrogram.raw.functions.phone.EditGroupCallParticipant(
+                #             call=call,
+                #             participant=peer,
+                #             muted=False
+                #         )
+                #     )
 
 
 try:
@@ -90,9 +140,10 @@ except KeyboardInterrupt:
     print('Exiting...')
 finally:
     try:
-        calls_app_simple.leave_group_call(
+        app_calls.leave_group_call(
             channel_id
         )
+        pass
     except KeyError:
         # странная ошибка из-за того, что я залогинился через канал
         pass
