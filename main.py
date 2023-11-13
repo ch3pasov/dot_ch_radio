69ce12cbb4cab62025d9f6a93caaed3b4819250a
Anatoliy Ch <50632903+ch3pasov@users.noreply.github.com>
2023-11-13 19:28:47 +0100

    Теперь радио логгирует события

--- main.py
+++ main.py
@@ -13,6 +13,23 @@ from pytgcalls.types import (
 import asyncio
 # from random import randint
 import secret
+import logging
+import sys
+
+formatter = logging.Formatter('%(asctime)s %(levelname)s [%(filename)s:%(lineno)s] %(message)s')
+handler_fancy_stdout = logging.StreamHandler(sys.stdout)
+handler_logger = logging.FileHandler("logs/common.log", mode='a')
+handler_fancy_stdout.setFormatter(formatter)
+handler_logger.setFormatter(formatter)
+# Корневой логгер. Должен ловить все ошибки и писать в файл.
+root = logging.getLogger()
+root.setLevel(logging.WARNING)
+root.addHandler(handler_fancy_stdout)
+root.addHandler(handler_logger)
+# Логгер для красивого принта. Почему он работает, не смотря на то, что root отлавливает только WARNING — не знаю.
+fancy_stdout = logging.getLogger(__name__)
+fancy_stdout.setLevel(logging.INFO)
+print = fancy_stdout.info
 
 api_id = secret.api_id
 api_hash = secret.api_hash
