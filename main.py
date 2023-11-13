0c5c692782f9a958cbe770f27854d09434c789ee
Anatoliy Ch <50632903+ch3pasov@users.noreply.github.com>
2023-11-14 00:32:47 +0100

    закоммитим то, чем богаты

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pytgcalls.types import (
     AudioQuality,
     # VideoParameters,
     # VideoQuality,
-    # Update,
+    Update,
 )
 import asyncio
 # from random import randint
@@ -39,29 +39,33 @@ dot_ch_chat_id = secret.dot_ch_chat_id
 dot_ch_radio_id = secret.dot_ch_radio_id
 dot_ch_radio_access_hash = secret.dot_ch_radio_access_hash
 
-app = pyrogram.Client("secret/my_account", api_id, api_hash)
+print('login in robot account')
+app_robot = pyrogram.Client("secret/robot_account", api_id, api_hash)
+app_robot.start()
 
-app_calls = PyTgCalls(app)
-app_calls.start()
-app_calls.join_group_call(
+print('login in dj account')
+app_dj = pyrogram.Client("secret/dj_account", api_id, api_hash)
+app_dj_calls = PyTgCalls(app_dj)
+app_dj_calls.start()
+
+
+app_dj_calls.join_group_call(
     dot_ch_id,
     join_as=pyrogram.raw.types.InputPeerChannel(channel_id=dot_ch_radio_id, access_hash=dot_ch_radio_access_hash)
 )
 
-print(1)
-
 
 # USE THIS IF YOU WANT SYNC WAY
-def get_youtube_stream():
+def get_youtube_stream(url='https://www.youtube.com/watch?v=jfKfPfyJRdk'):
     # USE THIS IF YOU WANT ASYNC WAY
-    async def run_async():
+    async def run_async(url=url):
         proc = await asyncio.create_subprocess_exec(
             'yt-dlp',
             '-g',
             '-f',
             # 'best[height<=?720][width<=?1280]',
             'ba',  # best audio
-            'https://www.youtube.com/live/jfKfPfyJRdk?si=gSkWIK09MCq5WuT1',
+            url,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE,
         )
@@ -70,31 +74,65 @@ def get_youtube_stream():
     return asyncio.get_event_loop().run_until_complete(run_async())
 
 
-remote = get_youtube_stream()
-
-app_calls.change_stream(
+app_dj_calls.change_stream(
     dot_ch_id,
     AudioPiped(
-        remote,
+        get_youtube_stream(),
         audio_parameters=AudioParameters.from_quality(AudioQuality.HIGH),
     )
 )
 
 
-# @app_calls.on_stream_end()
-# async def handler(client: PyTgCalls, update: Update):
-#     print(update)
-#     await app_calls.change_stream(
-#         dot_ch_id,
-#         AudioPiped(
-#             'audio_02.mp3',
-#             audio_parameters=AudioParameters.from_quality(AudioQuality.HIGH),
-#         )
-#     )
+@app_robot.on_message(pyrogram.filters.command(["pause"]) & pyrogram.filters.private)
+async def pause_handler(client, message):
+    await app_robot.send_message(message.from_user.id, await app_dj_calls.pause_stream(dot_ch_id))
+
+
+@app_robot.on_message(pyrogram.filters.command(["resume"]) & pyrogram.filters.private)
+async def resume_handler(client, message):
+    await app_robot.send_message(message.from_user.id, await app_dj_calls.resume_stream(dot_ch_id))
+
+
+@app_robot.on_message(pyrogram.filters.command(["time"]) & pyrogram.filters.private)
+async def time_handler(client, message):
+    await app_robot.send_message(message.from_user.id, await app_dj_calls.played_time(dot_ch_id))
+
+
+@app_robot.on_message(pyrogram.filters.command(["change_stream"]) & pyrogram.filters.private)
+async def change_stream_handler(client, message):
+    await app_dj_calls.change_stream(
+        dot_ch_id,
+        AudioPiped(
+            get_youtube_stream(url=message.command[1]),
+            audio_parameters=AudioParameters.from_quality(AudioQuality.HIGH),
+        )
+    )
+    await app_robot.send_message(
+        message.from_user.id,
+        "True?!"
+    )
+
+
+# сюда буду писать кастомную вещь
+@app_robot.on_message(pyrogram.filters.command(["please"]) & pyrogram.filters.private)
+async def please_handler(client, message):
+    print(asyncio.get_event_loop())
+
+
+@app_dj_calls.on_stream_end()
+async def handler(client: PyTgCalls, update: Update):
+    print(update)
+    await app_dj_calls.change_stream(
+        dot_ch_id,
+        AudioPiped(
+            get_youtube_stream(url='https://www.youtube.com/watch?v=jfKfPfyJRdk'),
+            audio_parameters=AudioParameters.from_quality(AudioQuality.HIGH),
+        )
+    )
 
 
 # главный обработчик событий в войсчате
-@app.on_raw_update()
+@app_dj.on_raw_update()
 async def raw(client, update, users, chats):
     # print(type(update))
     # print(dir(update))
@@ -120,8 +158,8 @@ async def raw(client, update, users, chats):
                 print(f'{participant_type} {participant_id} left')
             if participant.just_joined:
                 print(f'{participant_type} {participant_id} just joined')
-                peer = await app.resolve_peer(participant_id)
-                await app.invoke(
+                peer = await app_dj.resolve_peer(participant_id)
+                await app_dj.invoke(
                     pyrogram.raw.functions.phone.EditGroupCallParticipant(
                         call=call,
                         participant=peer,
@@ -130,10 +168,10 @@ async def raw(client, update, users, chats):
                 )
             if participant.raise_hand_rating:
                 print(f'{participant_type} {participant_id} raise hand with rating {participant.raise_hand_rating}')
-                # peer = await app.resolve_peer(participant_id)
+                # peer = await app_dj.resolve_peer(participant_id)
                 # await asyncio.sleep(5)
                 # if randint(0, 1):
-                #     await app.invoke(
+                #     await app_dj.invoke(
                 #         pyrogram.raw.functions.phone.EditGroupCallParticipant(
                 #             call=call,
                 #             participant=peer,
@@ -141,7 +179,7 @@ async def raw(client, update, users, chats):
                 #         )
                 #     )
                 # else:
-                #     await app.invoke(
+                #     await app_dj.invoke(
                 #         pyrogram.raw.functions.phone.EditGroupCallParticipant(
                 #             call=call,
                 #             participant=peer,
@@ -156,7 +194,7 @@ except KeyboardInterrupt:
     print('Exiting...')
 finally:
     try:
-        app_calls.leave_group_call(
+        app_dj_calls.leave_group_call(
             dot_ch_id
         )
         pass
