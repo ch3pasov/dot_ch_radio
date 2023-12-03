e72ce2c2671219f5d64ba90c4e3d674583177b5d
Anatoliy Ch <50632903+ch3pasov@users.noreply.github.com>
2023-12-03 17:12:48 +0100

    Основа для клики

---
+++ programs/clique.py
@@ -0,0 +1,6 @@
+import sqlite3
+# import re
+# import datetime
+
+connection = sqlite3.connect('/Users/anatoliy-ch/Documents/projects/dot_ch_radio/volume/database/common.db')
+cursor = connection.cursor()
