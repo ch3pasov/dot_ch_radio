5d12b2dab1d31d5d7eda759328e3cfa7486c0dda
Anatoliy Ch <50632903+ch3pasov@users.noreply.github.com>
2023-12-04 02:14:14 +0100

    экспериментирую с формой

--- volume/config_example/clique_config_example.py
+++ volume/config_example/clique_config_example.py
@@ -1,8 +1,8 @@
-initiation_phrase = 'Я вступаю в [㊙️] Клику:\nt.me/dot_ch_bot?start=clique'
-description_phrase = '[㊙️] Клика: t.me/dot_ch_bot?start=clique'
+initiation_phrase = 'Я вступаю в ㊙️ Клику:\nt.me/dot_ch_bot?start=clique'
+description_phrase = '㊙️ Клика: t.me/dot_ch_bot?start=clique'
 link_word = "㊙️"
 join_link = "https://t.me/dot_ch_bot?start=clique_join"
 minimum_channel_members_count = 10
 maximum_inactive_days = 90
 clique_folder_id = 2
-clique_folder_name = "[ТЕСТ][㊙️]"
+clique_folder_name = "[ТЕСТ]㊙️"
