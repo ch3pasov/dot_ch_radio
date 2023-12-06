6dbe17bdf24982c4fbb24ed06595c75c10f82eb6
Anatoliy Ch <50632903+ch3pasov@users.noreply.github.com>
2023-12-07 00:07:09 +0100

    delete legacy

--- volume/config_example/clique_config_example.py
+++ volume/config_example/clique_config_example.py
@@ -1,7 +1,5 @@
 initiation_phrase = 'Я вступаю в ㊙️ Клику: t.me/dot_ch_bot?start=clique'
 description_phrase = '㊙️ Клика: t.me/dot_ch_bot?start=clique'
-# link_word = "㊙️"
-# join_link = "https://t.me/dot_ch_bot?start=clique_join"
 minimum_channel_members_count = 10
 maximum_inactive_days = 90
 clique_folder_id = 2
