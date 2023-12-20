ba285a3b413afd0f5731178f312a564ac6f89203
Anatoliy Ch <50632903+ch3pasov@users.noreply.github.com>
2023-12-20 04:32:58 +0100

    smol fix

--- programs/other.py
+++ programs/other.py
@@ -35,7 +35,7 @@ async def get_weather(location):
 minecaft_server_info = """Присоединяйтесь к нашему Minecraft серверу!
 
 Адрес сервера: `{minecraft_server}`
-Карта сервера: {minecraft_server}:8100
+Карта сервера: http://{minecraft_server}:8100
 Статус сервера: {status}
 """
 status_online = """**Онлайн**
