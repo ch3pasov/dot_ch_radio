8d13e84e68cb3307644a558218ad5fe283fb65e8
Anatoliy Ch <50632903+ch3pasov@users.noreply.github.com>
2024-01-24 15:47:36 +0100

    better

--- programs/other.py
+++ programs/other.py
@@ -78,7 +78,7 @@ async def get_minecraft_server_info():
 
 nadezhdin_regex = r'                                <span class="progressbar__el__text">Собрано подписей: (\d+)( / 2500)?</span>'
 out = '''
-Считаю количество подписей с nadezhdin2024.ru/addresses с учётом [ограничения](https://www.consultant.ru/document/cons_doc_LAW_40445/709aad55b0103f1e128dde11785bbd41d4328966/) в 2500 на регион.
+Считаю количество подписей с nadezhdin2024.ru/addresses, потому что либералы не умеют.
 
 Всего собрано подписей: **{scores_all:,}**.
 С ограниченим в 2500 на регион подписей: **{scores_2500:,}**.
@@ -88,7 +88,7 @@ out = '''
 
 async def get_nadezhdin():
     text = await aiohttp_get('https://nadezhdin2024.ru/addresses', 'text')
-    scores_raw = [region_score[0] for region_score in re.findall(nadezhdin_regex, text)]
+    scores_raw = [int(region_score[0]) for region_score in re.findall(nadezhdin_regex, text)]
     scores_all = sum(scores_raw)
     scores_2500 = sum([min(2500, score) for score in scores_raw])
     return out.format(scores_all=scores_all, scores_2500=scores_2500)
