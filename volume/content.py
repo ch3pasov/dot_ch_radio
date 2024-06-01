05fed1270b5e841693481b7c64b484aac5dab628
Anatoliy Ch <50632903+ch3pasov@users.noreply.github.com>
2024-06-01 10:52:42 +0200

    deleted youtube

--- volume/content.py
+++ volume/content.py
@@ -417,8 +417,8 @@ common_tree = {
     }
 }
 
-startup_url = "https://youtu.be/miZHa7ZC6Z0"
-shutdown_url = "https://youtu.be/XvoVObL4bYY"
+startup_url = "https://zvukipro.com/uploads/files/2020-12/1609413715_the-microsoft-sound.mp3"
+shutdown_url = "https://zvukipro.com/uploads/files/2021-01/1609567191_shutdown.mp3"
 
 default_url = "https://live.lofiradio.ru/lofi_mp3_128"
 
