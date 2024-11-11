09a8485fb67d7bd729f05f9272f2650094ca4788
Anatoliy Ch <dev@anatoliy.ch>
2024-11-12 00:37:06 +0100

    picture in the middle test

--- volume/content.py
+++ volume/content.py
@@ -320,7 +320,7 @@ common_tree = {
                 },
                 "katakana_racism": {
                     "name": "🇯🇵 Руссуко-Японсукий пэрэводутику (простите)",
-                    "description": "[​​​​​​​​​​​](https://storage.yandexcloud.net/dot-ch-bot-bucket/Illustrations/%F0%9F%87%A8%F0%9F%87%B3%F0%9F%87%AF%F0%9F%87%B5.jpg)",
+                    "description": "Привет!\n[​​​​​​​​​​​](https://storage.yandexcloud.net/dot-ch-bot-bucket/Illustrations/%F0%9F%87%A8%F0%9F%87%B3%F0%9F%87%AF%F0%9F%87%B5.jpg)\nПока!",
                     "beta_access": 1,
                 },
                 "bashkir-haiku": {
