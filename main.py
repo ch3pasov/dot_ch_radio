d0ce67fa1b876f6b6de256bb7b353bb3ec7cf397
Anatoliy Ch <dev@anatoliy.ch>
2024-11-12 00:59:30 +0100

    oops

--- main.py
+++ main.py
@@ -223,7 +223,7 @@ async def answer_vasilii_game(client, message):
 
 
 # rus_to_katakana
-@app_robot.on_message(pyrogram.filters.command(["/rus_to_katakana"]) & pyrogram.filters.incoming)
+@app_robot.on_message(pyrogram.filters.command(["rus_to_katakana"]) & pyrogram.filters.incoming)
 async def answer_rus_to_katakana(client, message):
     await app_robot.send_message(
         message.chat.id,
